695a588e3707ea46d65c035995833d22dbd3a9e6
Sebastian <sebaas14toulier@gmail.com>
2021-04-19 22:03:43 -0500

    modal not working

--- js/app.js
+++ js/app.js
@@ -8,41 +8,51 @@ const derechaContent = document.getElementById("derechaContent");
 const imgCentro = document.getElementById("imgCentro");
 const imgIzquierda = document.getElementById("imgIzquierda");
 const descriptionDefault =
-  "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.";
+    "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.";
 
 window.addEventListener("load", () => {
-  if (screen.width <= 992) {
-    centroTitle.textContent = "Izquierda";
-    centroContent.textContent = descriptionDefault;
-    izquierdaTitle.textContent = "Centro";
-    izquierdaContent.textContent = descriptionDefault;
-    derechaTitle.textContent = "Derecha";
-    derechaContent.textContent = descriptionDefault;
-    imgIzquierda.src = "./img/Seccion_Student/exams.png";
-    imgCentro.src = "./img/Seccion_Student/professor.png";
-  } else {
-    centroTitle.textContent = "Centro";
-    centroContent.textContent = descriptionDefault;
-    izquierdaTitle.textContent = "Izquierda";
-    izquierdaContent.textContent = descriptionDefault;
-    derechaTitle.textContent = "Derecha";
-    derechaContent.textContent = descriptionDefault;
-    imgIzquierda.src = "./img/Seccion_Student/professor.png";
-    imgCentro.src = "./img/Seccion_Student/exams.png";
-  }
+    if (screen.width <= 992) {
+        centroTitle.textContent = "Izquierda";
+        centroContent.textContent = descriptionDefault;
+        izquierdaTitle.textContent = "Centro";
+        izquierdaContent.textContent = descriptionDefault;
+        derechaTitle.textContent = "Derecha";
+        derechaContent.textContent = descriptionDefault;
+        imgIzquierda.src = "./img/Seccion_Student/exams.png";
+        imgCentro.src = "./img/Seccion_Student/professor.png";
+    } else {
+        centroTitle.textContent = "Centro";
+        centroContent.textContent = descriptionDefault;
+        izquierdaTitle.textContent = "Izquierda";
+        izquierdaContent.textContent = descriptionDefault;
+        derechaTitle.textContent = "Derecha";
+        derechaContent.textContent = descriptionDefault;
+        imgIzquierda.src = "./img/Seccion_Student/professor.png";
+        imgCentro.src = "./img/Seccion_Student/exams.png";
+    }
 });
 
 window.addEventListener("resize", () => {
-  if (screen.width <= 992) {
-    izquierdaTitle.textContent = "Centro";
-    imgIzquierda.src = "./img/Seccion_Student/exams.png";
-    centroTitle.textContent = "Izquierda";
-    imgCentro.src = "./img/Seccion_Student/professor.png";
-  } else {
-    izquierdaTitle.textContent = "Izquierda";
-    imgIzquierda.src = "./img/Seccion_Student/professor.png";
-    centroTitle.textContent = "Centro";
-    imgCentro.src = "./img/Seccion_Student/exams.png";
-  }
+    if (screen.width <= 992) {
+        izquierdaTitle.textContent = "Centro";
+        imgIzquierda.src = "./img/Seccion_Student/exams.png";
+        centroTitle.textContent = "Izquierda";
+        imgCentro.src = "./img/Seccion_Student/professor.png";
+    } else {
+        izquierdaTitle.textContent = "Izquierda";
+        imgIzquierda.src = "./img/Seccion_Student/professor.png";
+        centroTitle.textContent = "Centro";
+        imgCentro.src = "./img/Seccion_Student/exams.png";
+    }
 });
 //Fin de Seccion_Student
+
+
+
+
+
+// *! to open modal suscription when the page loads
+
+$('#myModal').on('shown.bs.modal', function() {
+    $('#myInput').trigger('focus')
+})
